29cc6755107a830a91426d131549e597c32b20a7
adammarples <adammarples@gmail.com>
2022-01-05 15:19:33 +0000

    add pre-commit flake8, isort, black and mypy and fix errors

--- tests/test_rocksdbdict.py
+++ tests/test_rocksdbdict.py
@@ -1,21 +1,20 @@
-import tempfile
 import shutil
-
+import tempfile
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
-import rocksdb
 import pytest
+import rocksdb
 
 from rocksdbdict import RocksdbDict
 
 
 @pytest.fixture
 def db():
-    tempdir = tempfile.gettempdir() / Path('rocksdbdict/test')
+    tempdir = tempfile.gettempdir() / Path("rocksdbdict/test")
     tempdir.mkdir(parents=True)
     _db = RocksdbDict(tempdir)
-    for x, y in ((1, 2), ('3', '4')):
+    for x, y in ((1, 2), ("3", "4")):
         _db[x] = y
     yield _db
     shutil.rmtree(tempdir)
@@ -23,10 +22,10 @@ def db():
 
 @pytest.fixture
 def readonly_db():
-    tempdir = tempfile.gettempdir() / Path('rocksdbdict/test')
+    tempdir = tempfile.gettempdir() / Path("rocksdbdict/test")
     tempdir.mkdir(parents=True)
     _db = RocksdbDict(tempdir)
-    for x, y in ((1, 2), ('3', '4')):
+    for x, y in ((1, 2), ("3", "4")):
         _db[x] = y
     _readonly_db = RocksdbDict(tempdir, read_only=True)
     yield _readonly_db
@@ -34,15 +33,15 @@ def readonly_db():
 
 
 def test_keys(db):
-    assert list(db.keys()) == [1, '3']
+    assert list(db.keys()) == [1, "3"]
 
 
 def test_values(db):
-    assert list(db.values()) == [2, '4']
+    assert list(db.values()) == [2, "4"]
 
 
 def test_items(db):
-    assert list(db.items()) == [(1, 2), ('3', '4')]
+    assert list(db.items()) == [(1, 2), ("3", "4")]
 
 
 def test_contains(db):
@@ -53,10 +52,10 @@ def test_contains(db):
 def test_default_keys(db):
     result = db.get(1)
     assert result == 2
-    result = db.get(1, 'default')
+    result = db.get(1, "default")
     assert result == 2
-    result = db.get(100, 'default')
-    assert result == 'default'
+    result = db.get(100, "default")
+    assert result == "default"
 
 
 def test_raises_key_error(db):
@@ -66,13 +65,13 @@ def test_raises_key_error(db):
 
 def test_readonly_db_raises_on_write(readonly_db):
     with pytest.raises(rocksdb.errors.NotSupported):
-        readonly_db['key'] = 'value'
+        readonly_db["key"] = "value"
 
 
 def test_readonly_db_concurrent_access(readonly_db):
     with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = executor.map(readonly_db.get, [1]*10)
-    assert list(futures) == [2]*10
+        futures = executor.map(readonly_db.get, [1] * 10)
+    assert list(futures) == [2] * 10
 
 
 def test_len(db):
@@ -84,5 +83,5 @@ def test_delete(db):
     assert 1 not in db
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main()
